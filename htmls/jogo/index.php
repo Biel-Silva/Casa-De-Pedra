d7fc59ff0f291b633a363df9b8291245d56a14a9
_Biel Silva_ <68821856+Biel-Silva@users.noreply.github.com>
2023-02-25 13:47:53 -0300

    Update index.php

--- htmls/jogo/index.php
+++ htmls/jogo/index.php
@@ -1,15 +1,20 @@
 <!doctype HTML>
 <html lang="pt-br">
-	</head>
+	<head>
 	<title>Game - Casa de Pedra</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="style.css" rel="stylesheet">
-	<head>
+	</head>
+	
 	<body>
 		<div class="contblock">
 			<canvas class="canvas" width=800 height=400></canvas>
 		</div>
 	<script src="script.js"></script>
 	</body>
+	<?php 
+	echo "Olá mundo";
+	?>
+
 </html>
